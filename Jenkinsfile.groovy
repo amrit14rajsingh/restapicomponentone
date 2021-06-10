cd060e7dd53ff06897d31fb20ac67389d269506c
amritraj <amrit.raj@tesco.com>
2021-06-11 02:27:26 +0530

    Added jenkins.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,10 +8,7 @@ node('master') {
 pipeline {
     agent any
 
-    tools {
-        // Install the Maven version configured as "M3" and add it to the path.
-        maven "M3"
-    }
+
 
     stages {
         stage('Build') {
